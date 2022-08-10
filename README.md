062bc524c8173cc9a268fe5cdebcea5004bb86ea
Shawn Wagner <shawnw.mobile@gmail.com>
2022-08-09 22:33:28 -0700

    Fix documentation category

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 scapegoat-tree
 ==============
 
+[![Scribble](https://img.shields.io/badge/Docs-Scribble-blue.svg)](https://docs.racket-lang.org/scapegoat-tree/index.html)
 
 Various Racket modules implementing data structures backed by scapegoat trees.
 
--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "scapegoat-tree")
 (define deps '("data-lib" "base"))
 (define build-deps '("data-doc" "scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/scapegoat-tree.scrbl" (multi-page) ("Scapegoat Trees"))))
+(define scribblings '(("scribblings/scapegoat-tree.scrbl" (multi-page) ("Data Structures"))))
 (define pkg-desc "Dicts and Sets using Scapegoat Trees")
 (define version "1.0")
 (define pkg-authors '(shawnw))
